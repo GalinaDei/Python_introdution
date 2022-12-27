213e85491c885bdb08702a0a9b4922eb2f7aaa1d
Galina <gal-dej@yandex.ru>
2022-12-27 12:17:43 +0300

    Task_4.3

--- Seminar4/Task_4.3.py
+++ Seminar4/Task_4.3.py
@@ -1,5 +1,5 @@
-# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся
-# элементов исходной последовательности в том же порядке.
+# 3. Задайте последовательность чисел. Напишите программу, которая выведет список
+# неповторяющихся элементов исходной последовательности в том же порядке.
 # in                           in
 # 7                            -1
 # out                          out
@@ -8,20 +8,28 @@
 import random
 from collections import Counter
 
-def non_repeating_elements(num):
+
+def num_sequence(num):
     new_list = []
-    for i in range(1, num+1):
-        num1 = random.randint(0, 9)
-        new_list.append(num1)
-    print(new_list)
-    list2 = Counter(new_list)
-    print(list2.value(1))
-
-    return list2
-
-
-
-print(non_repeating_elements(int(input())))
+    if num < 0:
+        print("Negative value of the number of numbers!")
+    else:
+        for i in range(1, num + 1):
+            num1 = random.randint(0, 9)
+            new_list.append(num1)
+        print(new_list)
+    return new_list
 
 
+def non_repeating_elements(num_list):
+    list2 = []
+    list1 = Counter(num_list)
+    for k, v in list1.items():
+        if v == 1:
+            list2.append(k)
+    print(list2)
 
+
+array = num_sequence(int(input()))
+
+non_repeating_elements(array)
