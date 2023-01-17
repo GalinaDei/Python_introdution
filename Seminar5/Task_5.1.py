f1bfe499e94ef7d9998b6637ff1d64d1c0f09ed1
Galina <gal-dej@yandex.ru>
2023-01-17 16:37:50 +0300

    Task 5.2 has done

--- Seminar5/Task_5.1.py
+++ Seminar5/Task_5.1.py
@@ -9,13 +9,16 @@ import random
 
 
 def text_creater(number):
-    abc = ["абв","бав","вба", "ааа", "ббв", "абв", "баа"]
-    str = [''.join(random.choice(abc)) for i in range(number)]
-    print(str)
-    return str
+    if number > 0:
+        abc = ["абв","бав","вба", "ааа", "ббв", "абв", "баа"]
+        str = [''.join(random.choice(abc)) for i in range(number)]
+        print(str)
+        str1 = list(filter(lambda x: x != "абв", str))
+        print(str1)
+        return str1
+    else:
+        print("The data is incorrect")
 
 
 text = text_creater(int(input()))
 
-str = list(filter(lambda x: x != "абв", text))
-print(str)
