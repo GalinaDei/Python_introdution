f8278cb5c5d55ffdbfadfe0da55ad54785763a92
Galina <gal-dej@yandex.ru>
2023-01-19 16:13:07 +0300

    Task 6.1 has done

--- Seminar6/Task_6.1.py
+++ Seminar6/Task_6.1.py
@@ -5,3 +5,22 @@
 # out                                  out
 # [15, 16, 2, 3, 1, 7, 5, 4, 10]       [28, 20, 10, 5, 1, 24, 7, 15, 23, 25]
 # [16, 3, 7, 10]                       [24, 15, 23, 25]
+
+import random
+
+def list_create(num):
+    if num.isdigit() and int(num) > 1:
+        lst = [random.randint(0, 100) for i in range(int(num))]
+        return lst
+    else:
+        print("Введено екорректное значение")
+
+lst = list_create(input("Введите число > 2:  "))
+print(lst)
+
+def bigger_value_list(list):
+    if list:
+        lst = [list[i] for i in range(1, len(list)) if list[i] > list[i-1]]
+        print(lst)
+
+bigger_value_list(lst)
